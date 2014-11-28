f1d0c057479b20181138cb4475ad79084555eac6
meshmote <deepcmd@gmail.com>
2014-11-28 14:51:26 -0800

    Setting up superclass for users, and tweaking unit tests

--- test_ticker.py
+++ test_ticker.py
@@ -1,1 +1,42 @@
 __author__ = 'Robert W. Perkins'
+
+
+import pytest
+from ticker import TickerInc
+
+
+def set_up():
+    investment = 10000.0
+    inc_id = 10001
+    corp_name = "Test Corp"
+    pri_shares = 1000
+    pub_shares = 100
+    ticker_id = 20001
+    user_name = 'First User'
+    my_corp = TickerInc(investment, inc_id, corp_name, pri_shares, pub_shares, ticker_id, user_name)
+    return my_corp
+
+
+def test_object_exists():
+    my_corp = set_up()
+    assert isinstance(my_corp, TickerInc)
+
+
+def test_attrib_inits1():
+    my_corp = set_up()
+    assert my_corp.incorp_id == 10001
+    assert my_corp.ticker_id == 20001
+    #assert my_corp.incorp_id == 10001
+    #assert my_corp.incorp_id == 10001
+    #assert my_corp.incorp_id == 10001
+    #assert my_corp.incorp_id == 10001
+
+def test_getter():
+    my_corp = set_up()
+    assert my_corp.value == 1000.0
+
+
+#def test_setter():
+    #my_corp = set_up()
+    #my_corp.value = 20000.0
+    #assert my_corp.value == 20000.0
---
+++ ticker.py
@@ -0,0 +1,54 @@
+__author__ = 'Robert W. Perkins'
+
+
+class TickerUser(object):
+    def __init__(self, ticker_id, user_name):
+        #Define basic user attributes and transaction methods
+        self._ticker_id = ticker_id
+        self.user_name = user_name
+
+    @property
+    def ticker_id(self):
+        return self._ticker_id
+
+    @ticker_id.setter
+    def ticker_id(self, value):
+        self._ticker_id = value
+
+
+class TickerInc(TickerUser):
+    def __init__(self, init_investment, incorp_id, corp_name, pri_shares, pub_shares, ticker_id, user_name):
+        #Extend TickerUser class with methods and attribs of a company
+        self.init_investment = init_investment
+        self.name = corp_name
+        self._incorp_id = incorp_id
+        self._pri_shares = pri_shares
+        self._pub_shares = pub_shares
+        self._ticker_price = init_investment / pri_shares
+        super(TickerInc, self).__init__(ticker_id, user_name)
+
+    @property
+    def value(self):
+        #Return a simple valuation based on public and private share value
+
+        #  assumes public share price and private share price are the same
+        #  May add something more tricky later (e.g. discounted cash flow analogs)
+        return self._ticker_price * (self._pri_shares + self._pub_shares) - self.init_investment
+
+    @property
+    def ticker_price(self):
+        return self._ticker_price
+
+    @ticker_price.setter
+    def ticker_price(self, value):
+        self._ticker_price = value
+
+    @property
+    def incorp_id(self):
+        return self._incorp_id
+
+    @incorp_id.setter
+    def incorp_id(self, value):
+        self._incorp_id = value
+
+
