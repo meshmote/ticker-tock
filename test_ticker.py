b7436971f60a39fffcbea8df30f5040247a3f79f
meshmote <deepcmd@gmail.com>
2014-11-30 20:49:36 -0800

    Created unit tests for initializing markets, and intializing corps given an initialized market

--- test_ticker.py
+++ test_ticker.py
@@ -1,11 +1,17 @@
+from __future__ import unicode_literals
+import pytest
+from ticker import *
+
+
 __author__ = 'Robert W. Perkins'
 
 
-import pytest
-from ticker import TickerInc
+def tickermarket_setup():
+    t_market = TickerMarket()
+    return t_market
 
 
-def set_up():
+def tickerinc_setup():
     #For inits where a folio file exists
     investment = 10000.0
     inc_id = 10001
@@ -14,11 +20,11 @@ def set_up():
     pub_shares = 100
     ticker_id = 20001
     user_name = 'First User'
-    my_corp = TickerInc(investment, inc_id, corp_name, pri_shares, pub_shares, ticker_id, user_name)
+    my_corp = TickerInc(investment, inc_id, corp_name, pri_shares, pub_shares, ticker_id, user_name, my_market)
     return my_corp
 
 
-def set_up2():
+def tickerinc_setup2():
     #Different ticker_id for case where folio file does not exist
     investment = 10000.0
     inc_id = 10002
@@ -27,21 +33,34 @@ def set_up2():
     pub_shares = 100
     ticker_id = 20045
     user_name = 'Second User'
-    my_corp = TickerInc(investment, inc_id, corp_name, pri_shares, pub_shares, ticker_id, user_name)
+    my_corp = TickerInc(investment, inc_id, corp_name, pri_shares, pub_shares, ticker_id, user_name, my_market)
+    return my_corp
+
+
+def tickerinc_setup3(my_market):
+    #Setup to test initialization with an existing market object
+    investment = 10000.0
+    inc_id = 10012
+    corp_name = "Other Test Corp"
+    pri_shares = 1000
+    pub_shares = 100
+    ticker_id = 20045
+    user_name = 'Second User'
+    my_corp = TickerInc(investment, inc_id, corp_name, pri_shares, pub_shares, ticker_id, user_name, my_market)
     return my_corp
 
 
 def test_object_exists():
-    my_corp = set_up()
+    my_corp = tickerinc_setup()
     assert isinstance(my_corp, TickerInc)
 
 
 def test_attrib_inits():
-    my_corp = set_up()
+    my_corp = tickerinc_setup()
     assert my_corp.value == 1000.0
-    assert my_corp.incorp_id == 10001
+    assert my_corp.incorp_id == 10012
     assert my_corp.ticker_id == 20001
-    assert my_corp.corp_name == 'Test Corp'
+    assert my_corp.corp_name == 'Tweet Proxy'
     assert my_corp.pri_shares == 1000
     assert my_corp.pub_shares == 100
     assert my_corp.user_name == 'First User'
@@ -49,12 +68,31 @@ def test_attrib_inits():
 
 def test_read_todict():
     #Make sure 20001.txt is present and contains the string version of the assert before running this test
-    my_corp = set_up()
+    my_corp = tickerinc_setup()
     assert my_corp.ticker_folio == {'Scrub Dub': 15, 'Drone Pies': 30, 'Tweet Proxy': 25, 'Robo Nanny': 100}
 
 
 def test_write_fromdict():
     #make sure 20045.txt is not present before running this test (should be a way to automate this)
-    my_corp = set_up2()
+    my_corp = tickerinc_setup2()
     assert my_corp.ticker_folio == {}
     assert my_corp.folio_path.is_file()
+
+
+def test_ticker_marketinit():
+    t_market = tickermarket_setup()
+    assert isinstance(t_market, TickerMarket)
+    assert type(t_market.member_incs) is dict
+    assert t_market.member_incs == {u'10010': 'Scrub Dub', u'10011': 'Drone Pies', u'10012': 'Tweet Proxy',
+                                    u'10013': 'Robo Nanny'}
+    assert t_market.close_price == {u'10010': u'10.0', u'10011': u'35.0', u'10012': u'120.0', u'10013': u'5.0'}
+    assert t_market.dayavg_price == {u'10010': u'10.0', u'10011': u'35.0', u'10012': u'120.0', u'10013': u'5.0'}
+    assert t_market.dayvolume == {u'10010': 0, u'10011': 0, u'10012': 0, u'10013': 0}
+
+
+def test_corpinit2market():
+    t_market = tickermarket_setup()
+    my_corp = tickerinc_setup3(t_market)
+    assert my_corp.ticker_closeprice == unicode(120.0)
+    assert my_corp.ticker_dayavg_price == unicode(120.0)
+    assert my_corp.ticker_dayvolume == 0
--- ticker.py
+++ ticker.py
@@ -1,8 +1,9 @@
-__author__ = 'Robert W. Perkins'
-
+from __future__ import unicode_literals
 from pathlib import *
-import ast
 import json
+
+
+__author__ = 'Robert W. Perkins'
 
 
 def read_folio(ticker_id):
@@ -28,6 +29,23 @@ def get_folio(path, ticker_id):
         return folio_dict
 
 
+def read_memberincs():
+    #Read mem_incs file, then create and return dict object
+    lit_path = '{f_name}{f_ext}'.format(f_name='mem_incs', f_ext='.txt')
+    return json.load(open(lit_path))
+
+
+def write_memberincs(memincs_dict):
+    lit_path = '{f_name}{f_ext}'.format(f_name='mem_incs', f_ext='.txt')
+    return json.dump(memincs_dict, open(lit_path, 'w'))
+
+
+def read_closeprice():
+    #Read mem_incs file, then create and return dict object
+    lit_path = '{f_name}{f_ext}'.format(f_name='close_price', f_ext='.txt')
+    return json.load(open(lit_path))
+
+
 class TickerUser(object):
 
     def __init__(self, ticker_id, user_name):
@@ -49,14 +67,16 @@ class TickerUser(object):
 
 
 class TickerInc(TickerUser):
-    def __init__(self, init_investment, incorp_id, corp_name, pri_shares, pub_shares, ticker_id, user_name):
+    def __init__(self, init_investment, incorp_id, corp_name, pri_shares, pub_shares, ticker_id, user_name, my_market):
         #Extend TickerUser class with methods and attribs of a company
         self.init_investment = init_investment
         self.corp_name = corp_name
-        self._incorp_id = incorp_id
+        self.incorp_id = incorp_id
+        self.ticker_closeprice = my_market.close_price[unicode(incorp_id)]
+        self.ticker_dayavg_price = my_market.dayavg_price[unicode(incorp_id)]
+        self.ticker_dayvolume = my_market.dayvolume[unicode(incorp_id)]
         self.pri_shares = pri_shares
         self.pub_shares = pub_shares
-        self._ticker_price = init_investment / pri_shares
         super(TickerInc, self).__init__(ticker_id, user_name)
 
     @property
@@ -65,22 +85,15 @@ class TickerInc(TickerUser):
 
         #  assumes public share price and private share price are the same
         #  May add something more tricky later (e.g. discounted cash flow analogs)
-        return self._ticker_price * (self.pri_shares + self.pub_shares) - self.init_investment
-
-    @property
-    def ticker_price(self):
-        return self._ticker_price
-
-    @ticker_price.setter
-    def ticker_price(self, value):
-        self._ticker_price = value
-
-    @property
-    def incorp_id(self):
-        return self._incorp_id
-
-    @incorp_id.setter
-    def incorp_id(self, value):
-        self._incorp_id = value
+        return self.ticker_closeprice * (self.pri_shares + self.pub_shares) - self.init_investment
 
 
+class TickerMarket(object):
+
+    def __init__(self):
+        #Read list of registered incs, get yesterday's closing prices, set initial daily average price to
+        #yesterday's close, and set daily volume to zero
+        self.member_incs = read_memberincs()
+        self.close_price = read_closeprice()
+        self.dayavg_price = self.close_price
+        self.dayvolume = {i: 0 for i in self.member_incs.iterkeys()}
